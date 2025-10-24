2317468c6af15331b8f4e86843a9cba48285965d
“Srinivasan <spugalenthi@chathamfinancial.com>
2025-10-24 14:17:43 +0530

    Add SQL query to Source Var

--- sql queries.sql
+++ sql queries.sql
@@ -5,3 +5,4 @@ Select * from users where age > 30;
 Select * from users where age > 30;
 
 Select name, email from users where subscribed = true;ct name, email from users where subscribed = true;
+Select {{ source('', '') }}
