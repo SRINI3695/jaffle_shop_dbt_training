6dbc221423a8b56351160b8a14be0852d0a13f84
AP Srini <104565677+SRINI3695@users.noreply.github.com>
2025-10-24 15:26:22 +0530

    new query

--- sql queries.sql
+++ sql queries.sql
@@ -7,3 +7,4 @@ Select * from users where age > 30;
 Select name, email from users where subscribed = true;ct name, email from users where subscribed = true;
 Select {{ source('', '') }}
 where is the docker
+here it is running;
