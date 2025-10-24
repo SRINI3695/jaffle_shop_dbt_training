062d0bbd8c10904fa097165522786cdca755f41b
“Srinivasan <spugalenthi@chathamfinancial.com>
2025-10-24 14:18:32 +0530

    Add SQL query to Source Var

--- sql queries.sql
+++ sql queries.sql
@@ -6,3 +6,4 @@ Select * from users where age > 30;
 
 Select name, email from users where subscribed = true;ct name, email from users where subscribed = true;
 Select {{ source('', '') }}
+where is the docker
