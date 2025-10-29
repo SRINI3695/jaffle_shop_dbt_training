35495f3cbcfb2cee8a439398c9fec733022394dd
Srinivasan Pugalenthi <spugalenthi@cf.com>
2025-10-29 06:59:16 +0000

    new changes

--- Models/Mart/jaffle_shop/customer.sql
+++ Models/Mart/jaffle_shop/customer.sql
@@ -6,4 +6,11 @@ with customers as (
 
 orders as (
     select * from {{ ref('stg_orders') }}
-),
+)
+select
+    customers.id as customer_id,
+    customers.name as customer_name,
+    count(orders.id) as number_of_orders
+from customers
+left join orders on customers.id = orders.customer
+group by customers.id, customers.name
